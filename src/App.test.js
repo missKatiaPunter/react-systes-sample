47f596b5dbadeb38a975541d492cc5267cfd7d6b
Katia <katia.punter@storyshareplatform.com>
2022-09-15 22:16:21 +0100

    Haven't pulled by mistake

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test("the initial input box is empty", ()=>{
@@ -6,4 +7,11 @@ test("the initial input box is empty", ()=>{
   const tempToConvertInput = screen.getByRole('textbox');
   // there is no role called password
   expect(tempToConvertInput.value).toBe('');
-})
+});
+
+test("that the user can type an email", () => {
+  render(<App/>);
+  const tempToConvertInput = screen.getByRole('textbox');
+  userEvent.type(tempToConvertInput, '23');
+  expect(tempToConvertInput.value).toBe('23');
+});
